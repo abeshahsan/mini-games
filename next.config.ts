050dc2dafc5d6cb4ef5e6ba021a9b7d2aecbf833
abeshahsan <abeshahsan2002@gmail.com>
2026-02-01 19:21:08 +0600

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["localhost", "10.112.231.22"],
 };
 
 export default nextConfig;
