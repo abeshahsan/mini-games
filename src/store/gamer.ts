97a85a3432939bddd043da795faf2f58c8868ca9
abeshahsan <abeshahsan2002@gmail.com>
2026-02-08 20:40:12 +0600

    feat: add zustand for state management and implement useGamerStore for user data handling

---
+++ src/store/gamer.ts
@@ -0,0 +1,30 @@
+import { Gamer } from "@/types";
+import { create } from "zustand";
+
+type GamerState = {
+	gamer: Gamer | null;
+	fetchGamer: () => Promise<void>;
+};
+
+export const useGamerStore = create<GamerState>((set) => ({
+	gamer: null,
+	fetchGamer: async () => {
+		try {
+			const response = await fetch("/api/me");
+			if (!response.ok) {
+				set({ gamer: null });
+				return;
+			}
+			const data = await response.json();
+			set({
+				gamer: {
+					id: data.uid,
+					ign: data.username,
+				},
+			});
+		} catch (e) {
+			console.log("Error fetching user:", e);
+			set({ gamer: null });
+		}
+	},
+}));
